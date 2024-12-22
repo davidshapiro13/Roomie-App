a0554b8c427b6ad171aa2cb8ea79cea60bbd166d
davidshapiro13 <davidshapiro@brandeis.edu>
2024-12-21 23:30:37 -0500

    hid config

--- app/Database.js
+++ app/Database.js
@@ -1,16 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { firebaseConfig } from '../secret';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCNuU3HvsJPY7oxo2_7q4O8I3YJyGGg-70",
-    authDomain: "roomie-app-bf9cf.firebaseapp.com",
-    projectId: "roomie-app-bf9cf",
-    storageBucket: "roomie-app-bf9cf.firebasestorage.app",
-    messagingSenderId: "757312119596",
-    appId: "1:757312119596:web:74fb7f0172492529480289",
-    measurementId: "G-8X307H6FR6"
-  };
 
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
